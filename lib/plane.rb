926cdff39b16465d5193fd203411da039b9ced4b
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-28 08:47:05 +0000

    Add take_off method

--- lib/plane.rb
+++ lib/plane.rb
@@ -4,16 +4,21 @@ class Plane
   attr_reader :location
 
   def initialize
-    @location = :grounded
+    @location = :landed
   end
 
   def land(airport)
     :landed
-    @location = :grounded
+    @location = :landed
+  end
+
+  def take_off
+    @location = :flying
   end
 
   def in_airport?
-    return false if @location == :airborne
-    true
+    return false if @location == :flying
+    return true if @location == :landed
   end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -6,8 +6,18 @@ describe Plane do
   let (:airport) {Airport.new}
   describe '#land' do
     it 'lands at an airport' do
-      expect(subject.land(airport)).to eq :grounded  
+      expect(subject.land(airport)).to eq :landed  
     end
   end
 
+  describe '#take_off' do
+    it 'takes off from an airport' do
+      expect(subject.take_off).to eq :flying
+    end
+
+    it 'is no londer in the airport' do
+        subject.take_off
+        expect(subject).not_to be_in_airport
+    end
+  end
 end
