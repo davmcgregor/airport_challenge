00647164f37c022dfe008e3edf0b25e14c47f472
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-01-28 08:22:01 +0000

    Add instance variable

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,19 @@
+require_relative 'airport'
+
 class Plane
-  def land
+  attr_reader :location
+
+  def initialize
+    @location = :grounded
+  end
+
+  def land(airport)
+    :landed
+    @location = :grounded
+  end
+
+  def in_airport?
+    return false if @location == :airborne
+    true
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,4 +2,12 @@ require 'plane'
 
 describe Plane do
   it { is_expected.to respond_to(:land) }
+
+  let (:airport) {Airport.new}
+  describe '#land' do
+    it 'lands at an airport' do
+      expect(subject.land(airport)).to eq :grounded  
+    end
+  end
+
 end
